5db880b21c8bafcf895f331a6d281a79b65a533e
Prateema1 <prateema.adhikari@gmail.com>
2023-01-25 23:35:15 +0530

    Added basic express routes for CRUD operations

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log("Hello BackEnd");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+const goalRoutes = require('./routes/goalRoutes')
+
+const app = express();
+
+app.use('/api/goals', goalRoutes )
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
