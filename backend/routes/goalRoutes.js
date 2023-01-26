688813aec5d06ea101f975f46b58c15e7c603823
Prateema1 <prateema.adhikari@gmail.com>
2023-01-26 14:44:59 +0530

    Added controller and middleware for handling routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,20 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const { getGoals, setGoals, updateGoals, deleteGoals } = require('../controllers/goalController')
 
-router.get('/', (req, res) => {
-    res.status(200).json({message: "Get Goals"});
-})
+// router.get('/', getGoals);
+// router.post('/' , setGoals);
+// Chaining same routes
+router.route('/').get(getGoals).post(setGoals)
 
-router.post('/', (req, res) => {
-    res.status(200).json({message: "Set Goals"});
-})
-
-router.put('/:id', (req, res) => {
-    res.status(200).json({message: `Update goal ${req.params.id}`});
-})
-
-router.delete('/:id', (req, res) => {
-    res.status(200).json({message: `Delete goal for ${req.params.id}`});
-})
+// router.put('/:id', updateGoals); 
+// router.delete('/:id', deleteGoals);
+//Chaining same routes
+router.route('/:id').delete(deleteGoals).put(updateGoals)
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000;
 const goalRoutes = require('./routes/goalRoutes')
 
 const app = express();
 
-app.use('/api/goals', goalRoutes )
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/api/goals', goalRoutes)
+
+//Overrides default express error handler
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
