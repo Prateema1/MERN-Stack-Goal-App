0c4d79b04ade56fee63b3cb28bc43801ea98709d
Prateema1 <prateema.adhikari@gmail.com>
2023-02-04 23:36:08 +0530

    Added dummy user routes and bcryptjs, jsonwebtoken.

---
+++ backend/controllers/userController.js
@@ -0,0 +1,29 @@
+//@desc  Register New User
+//@route POST /api/users
+//@access Public
+
+const registerUser = (req, res) => {
+    res.json({message : 'Register User'})
+}
+
+//@desc  Authenticate a User
+//@route POST /api/users/login
+//@access Public
+
+const loginUser = (req, res) => {
+    res.json({message : 'Login User'})
+}
+
+//@desc  Get User Data
+//@route POST /api/users/me
+//@access Public
+
+const getMe = (req, res) => {
+    res.json({message : 'User Data Display'})
+}
+
+module.exports = {
+    registerUser,
+    loginUser,
+    getMe
+}
