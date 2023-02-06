6d6f7e489d6fa1a5dbb386a6f50cd6c4e32e6274
Prateema1 <prateema.adhikari@gmail.com>
2023-02-06 22:57:08 +0530

    Added authentication and authorization using json web token, protected routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const { getGoals, setGoals, updateGoals, deleteGoals } = require('../controllers/goalController')
-
+const { protect } = require('../middleware/authMiddleware');
 // router.get('/', getGoals);
 // router.post('/' , setGoals);
 // Chaining same routes
-router.route('/').get(getGoals).post(setGoals)
+router.route('/').get(protect, getGoals).post(protect, setGoals)
 
 // router.put('/:id', updateGoals); 
 // router.delete('/:id', deleteGoals);
 //Chaining same routes
-router.route('/:id').delete(deleteGoals).put(updateGoals)
+router.route('/:id').delete(protect, deleteGoals).put(protect, updateGoals)
 
 module.exports = router;
